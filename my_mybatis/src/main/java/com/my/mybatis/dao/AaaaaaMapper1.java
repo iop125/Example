3fa480888a2752e6943c0de731fa1b685c9cffa5
缪宇 <miaoyu1@jd.com>
2019-04-18 10:31:47 +0800

    可配置的mapper

---
+++ my_mybatis/src/main/java/com/my/mybatis/dao/AaaaaaMapper1.java
@@ -0,0 +1,10 @@
+package com.my.mybatis.dao;
+
+import com.my.mybatis.entity.Aaaaaa;
+
+public interface AaaaaaMapper1 {
+
+    Aaaaaa getA(String aa);
+
+    void insert(Aaaaaa aaaaaa);
+}
--- my_mybatis/src/main/java/com/my/mybatis/main/Test1.java
+++ my_mybatis/src/main/java/com/my/mybatis/main/Test1.java
@@ -1,8 +1,7 @@
 package com.my.mybatis.main;
 
-import com.my.mybatis.dao.AaaaaaDao;
+import com.my.mybatis.dao.AaaaaaMapper1;
 import com.my.mybatis.entity.Aaaaaa;
-import com.sun.imageio.plugins.common.InputStreamAdapter;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,19 +9,25 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 
 public class Test1 {
 
     public static void main(String[] args) throws IOException {
         //mybatis的配置文件
 
-        Aaaaaa a= new Aaaaaa();
-        a.setId(1121L);
+        Aaaaaa a = new Aaaaaa();
+        a.setId(new Date().getTime());
         a.setAa("11111");
         InputStream inputStream = Resources.getResourceAsStream("myBatis-config.xml");
-        SqlSessionFactory factory = new SqlSessionFactoryBuilder() .build(inputStream);
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = factory.openSession();
-        sqlSession.insert("AaaaaaDao.insert",a);
+        sqlSession.insert("AaaaaaDao.insert", a);
+        Aaaaaa a1 = new Aaaaaa();
+        a.setId(new Date().getTime());
+        a.setAa("11111");
+        AaaaaaMapper1 mapper = sqlSession.getMapper(AaaaaaMapper1.class);
+        mapper.insert(a1);
         sqlSession.commit();
         sqlSession.close();
     }

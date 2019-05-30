315c28ef1f2283b599ff64a00b086835068d68ec
iop125 <232940202@qq.com>
2019-05-31 00:53:33 +0800

    B树

--- javaex/src/main/java/com/example/javaex/javaex/dataStructure/tree/BTree/BTreeNode.java
+++ javaex/src/main/java/com/example/javaex/javaex/dataStructure/tree/BTree/BTreeNode.java
@@ -44,7 +44,7 @@ public class BTreeNode {
             } else {
                 return ptrs.get(0).search(information);
             }
-        } else if (weizhi == informations.size() - 1) {
+        } else if (weizhi ==  - 1) {
             if (ptrs.get(ptrs.size() - 1) == null) {
                 return this;
             } else {
@@ -63,7 +63,7 @@ public class BTreeNode {
         int weizhi = -1;
         for (int i = 0; i < informations.size(); i++) {
             Integer value = informations.get(i);
-            if (value > information) {
+            if (value >=information) {
                 weizhi = i;
                 break;
             }
@@ -71,27 +71,57 @@ public class BTreeNode {
         return weizhi;
     }
 
-    public void addValue(Integer information,int m) {
-        this.informations.add(information);
+    public void addValue(BTreeNode information,int m) {
+        this.informations.add(information.informations.get(0));
         Collections.sort(informations);
         if (informations.size() >= m - 1) {
-            int zhong = (m-1) / 2;
-            Integer newBTreeNodeValue = informations.get(zhong);
+            int zhong = m%2==0?m/2:m/2+1;
+            Integer newBTreeNodeValue = informations.get(zhong-1);
             BTreeNode newBTreeNode = new BTreeNode(null, newBTreeNodeValue);
             BTreeNode newBTreeNodeMax = null;
+            BTreeNode newBTreeNodemin = null;
             for (int i = 0; i < informations.size() - 1; i++) {
-                if (i > zhong) {
+                if (i > zhong-1) {
                     //生成右树
                     if (newBTreeNodeMax == null) {
                         newBTreeNodeMax = new BTreeNode(newBTreeNode, informations.get(i));
                     } else {
                         newBTreeNodeMax.add(informations.get(i),m);
                     }
-                    //生成左树
-                    this
-
                 }
-
+                if (i < zhong-1) {
+                    //生成左树
+                    if (newBTreeNodemin == null) {
+                        newBTreeNodemin = new BTreeNode(newBTreeNode, informations.get(i));
+                    } else {
+                        newBTreeNodemin.add(informations.get(i),m);
+                    }
+                }
+            }
+            if(this.parent !=null){
+                newBTreeNode.ptrs.add(newBTreeNodemin);
+                newBTreeNode.ptrs.add(newBTreeNodeMax);
+                this.parent.addValue( newBTreeNode, m);
+            }else{
+                this.ptrs.add(newBTreeNodemin);
+                this.ptrs.add(newBTreeNodeMax);
+                this.informations.add(information.informations.get(0));
+            }
+        }
+        //新添加的节点要和原有节点重合
+        int weizhi = searchWeiZhi( this.informations, information.informations.get(0));
+        List<BTreeNode> ptrs = new ArrayList<>();//指针节点
+        int ptrsLength = this.ptrs.size();//指针节点
+        int n = 0;
+        for(int i=0;i< ptrsLength;i++){
+            if(i ==weizhi){
+                ptrs.add(information.ptrs.get(0));
+            }else
+            if(i==weizhi+1){
+                ptrs.add(information.ptrs.get(1));
+            }else{
+                ptrs.add(this.ptrs.get(n));
+                n++;
             }
         }
     }

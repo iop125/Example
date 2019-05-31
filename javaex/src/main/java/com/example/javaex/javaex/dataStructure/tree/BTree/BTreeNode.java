afab2aa26277a805881781c5cb8ca779a32c3ea1
缪宇 <miaoyu1@jd.com>
2019-05-31 23:28:10 +0800

    构建b树和根据值查找到节点

--- javaex/src/main/java/com/example/javaex/javaex/dataStructure/tree/BTree/BTreeNode.java
+++ javaex/src/main/java/com/example/javaex/javaex/dataStructure/tree/BTree/BTreeNode.java
@@ -10,51 +10,45 @@ public class BTreeNode {
     List<BTreeNode> ptrs = new ArrayList<>();//指针节点
     List<Integer> informations = new ArrayList<Integer>();//关键字--值
 
-    public BTreeNode(BTreeNode parent, Integer information) {
+    public BTreeNode(Integer information) {
         if (information != null) {
             this.informations.add(information);
-            this.parent = parent;
-        } else {
-            this.parent = null;
         }
+    }
+
+    public BTreeNode() {
     }
 
     /**
      * @param information 要添加的数据
      * @param m           该树的阶
      */
-    public void add(Integer information,int m) {
-        //先查找 到需要添加的节点
-
-        BTreeNode  bTreeNode  = search( information);
+    public void add(Integer information, int m) {
 
 
-
-
-        //在那个节点上添加数据
-
-
-    }
-
-    public BTreeNode search(Integer information) {
+        if (ptrs == null || ptrs.size() == 0) {
+            this.addValue(new BTreeNode(information), m);
+            return;
+        }
+        //若不是叶子节点找到可以添加该关键字的节点添加
         int weizhi = searchWeiZhi(informations, information);
-        if (weizhi == 0) {
-            if (ptrs.get(0) == null) {
-                return this;
+        if (weizhi != -1) {
+            if (ptrs.get(weizhi) == null) {
+                //TODO 能走么这个直接添加
+                this.addValue(new BTreeNode(information), m);
             } else {
-                return ptrs.get(0).search(information);
-            }
-        } else if (weizhi ==  - 1) {
-            if (ptrs.get(ptrs.size() - 1) == null) {
-                return this;
-            } else {
-                return ptrs.get(ptrs.size() - 1).search(information);
+                ptrs.get(weizhi).parent = this;
+                ptrs.get(weizhi).add(information, m);
+                ptrs.get(weizhi).parent = null;
             }
         } else {
-            if (ptrs.get(weizhi) == null) {
-                return this;
+            if (ptrs.get(ptrs.size() - 1) == null) {
+                //TODO 这个直接添加
+                this.addValue(new BTreeNode(information), m);
             } else {
-                return ptrs.get(weizhi).search(information);
+                ptrs.get(ptrs.size() - 1).parent = this;
+                ptrs.get(ptrs.size() - 1).add(information, m);
+                ptrs.get(ptrs.size() - 1).parent = null;
             }
         }
     }
@@ -63,7 +57,7 @@ public class BTreeNode {
         int weizhi = -1;
         for (int i = 0; i < informations.size(); i++) {
             Integer value = informations.get(i);
-            if (value >=information) {
+            if (value >= information) {
                 weizhi = i;
                 break;
             }
@@ -71,58 +65,85 @@ public class BTreeNode {
         return weizhi;
     }
 
-    public void addValue(BTreeNode information,int m) {
-        this.informations.add(information.informations.get(0));
+    public void addValue(BTreeNode addNode, int m) {
+        //在已经找到的大节点中 添加该元素（根节点或者叶子节点）
+        this.informations.add(addNode.informations.get(0));
+        //对队列排序
         Collections.sort(informations);
-        if (informations.size() >= m - 1) {
-            int zhong = m%2==0?m/2:m/2+1;
-            Integer newBTreeNodeValue = informations.get(zhong-1);
-            BTreeNode newBTreeNode = new BTreeNode(null, newBTreeNodeValue);
+        //处理指针
+        if (ptrs.size() > 0) {
+            int newWeizhi = -1;
+            for (int i = 0; i < informations.size(); i++) {
+                if (informations.get(i) == addNode.informations.get(0)) {
+                    newWeizhi = i;
+                    break;
+                }
+            }
+            int n = 0;
+            int ptrsLength = this.ptrs.size();//指针节点
+            List<BTreeNode> newPtrs = new ArrayList<>();//指针节点
+            for (int i = 0; i < ptrsLength + 1; i++) {
+                if (i == newWeizhi) {
+                    newPtrs.add(addNode.ptrs.get(0));
+                } else if (i == newWeizhi + 1) {
+                    newPtrs.add(addNode.ptrs.get(1));
+                } else {
+                    newPtrs.add(this.ptrs.get(n));
+                    n++;
+                }
+            }
+            this.ptrs = newPtrs;
+        }
+
+        if (informations.size() > m - 1) {
+            int zhong = m % 2 == 0 ? m / 2 : m / 2 + 1;
+            Integer newBTreeNodeValue = informations.get(zhong - 1);
+            BTreeNode newBTreeNode = new BTreeNode(newBTreeNodeValue);
             BTreeNode newBTreeNodeMax = null;
             BTreeNode newBTreeNodemin = null;
-            for (int i = 0; i < informations.size() - 1; i++) {
-                if (i > zhong-1) {
+            for (int i = 0; i < informations.size(); i++) {
+                if (i > zhong - 1) {
                     //生成右树
                     if (newBTreeNodeMax == null) {
-                        newBTreeNodeMax = new BTreeNode(newBTreeNode, informations.get(i));
+                        newBTreeNodeMax = new BTreeNode(informations.get(i));
                     } else {
-                        newBTreeNodeMax.add(informations.get(i),m);
+                        newBTreeNodeMax.add(informations.get(i), m);
                     }
                 }
-                if (i < zhong-1) {
+                if (i < zhong - 1) {
                     //生成左树
                     if (newBTreeNodemin == null) {
-                        newBTreeNodemin = new BTreeNode(newBTreeNode, informations.get(i));
+                        newBTreeNodemin = new BTreeNode(informations.get(i));
                     } else {
-                        newBTreeNodemin.add(informations.get(i),m);
+                        newBTreeNodemin.add(informations.get(i), m);
                     }
                 }
             }
-            if(this.parent !=null){
-                newBTreeNode.ptrs.add(newBTreeNodemin);
-                newBTreeNode.ptrs.add(newBTreeNodeMax);
-                this.parent.addValue( newBTreeNode, m);
-            }else{
-                this.ptrs.add(newBTreeNodemin);
-                this.ptrs.add(newBTreeNodeMax);
-                this.informations.add(information.informations.get(0));
+            newBTreeNode.ptrs.add(newBTreeNodemin);
+            newBTreeNode.ptrs.add(newBTreeNodeMax);
+            //2个子树的节点
+            if (this.ptrs.size() > 0) {
+                //newBTreeNodemin
+                newBTreeNodemin.ptrs = this.ptrs.subList(0, zhong);
+                //newBTreeNodeMax
+                newBTreeNodeMax.ptrs = this.ptrs.subList(zhong , this.ptrs.size());
+            }
+            if (this.parent != null) {
+                this.parent.addValue(newBTreeNode, m);
+            } else {
+                this.ptrs=newBTreeNode.ptrs;
+                this.informations = newBTreeNode.informations;
             }
         }
-        //新添加的节点要和原有节点重合
-        int weizhi = searchWeiZhi( this.informations, information.informations.get(0));
-        List<BTreeNode> ptrs = new ArrayList<>();//指针节点
-        int ptrsLength = this.ptrs.size();//指针节点
-        int n = 0;
-        for(int i=0;i< ptrsLength;i++){
-            if(i ==weizhi){
-                ptrs.add(information.ptrs.get(0));
-            }else
-            if(i==weizhi+1){
-                ptrs.add(information.ptrs.get(1));
-            }else{
-                ptrs.add(this.ptrs.get(n));
-                n++;
-            }
-        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "BTreeNode{" +
+                "parent=" + parent +
+                ", ptrs=" + ptrs +
+                ", informations=" + informations +
+                '}';
     }
 }
--- javaex/src/main/java/com/example/javaex/javaex/dataStructure/tree/BTree/BTreeTree.java
+++ javaex/src/main/java/com/example/javaex/javaex/dataStructure/tree/BTree/BTreeTree.java
@@ -1,12 +1,31 @@
 package com.example.javaex.javaex.dataStructure.tree.BTree;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class BTreeTree {
 
     BTreeNode root;
     int jie;
+
+    public BTreeTree(int jie) {
+        this.jie = jie;
+    }
+
+
+    public void add(Integer value) {
+        if (root == null) {
+            root = new BTreeNode();
+        }
+        root.add(value, jie);
+    }
+
+    public static void main(String a[]) {
+        BTreeTree btree = new BTreeTree(3);
+        int[] array = new int[]{200,100,300,400,600,800,900,850,860,1000,1300,1234,1500,1600,1700};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("-----   "+btree.root);
+            btree.add(array[i]);
+        }
+        System.out.println("-----   "+btree.root);
+    }
 
 }
